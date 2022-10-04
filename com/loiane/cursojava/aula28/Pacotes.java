eb77d927b1c0c1434d2d4e508488d978035e1211
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-10-03 21:10:12 -0300

    Adiciona Pacotes

---
+++ com/loiane/cursojava/aula28/Pacotes.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula28;
+
+import com.loiane.cursojava.aula27.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        Carro carro01;
+        com.loiane.cursojava.aula26.Carro carro02;
+    }
+}
